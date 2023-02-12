ab25e60ec1559705a7e26e6971bd5ac992d19b8d
airpilot07 <john.p.20071@gmail.com>
2023-02-12 16:54:09 +0200

    another commit

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,6 @@
 
 RH_RF95 rf96(RFM_CS, RFM_G0);
 Sd2Card scard;
-SdVolume sdsize;
 File coldata;
 
 void setup() {
@@ -35,7 +34,7 @@ void setup() {
   if (rf96.init())
     {
       Serial.println("RFM initialized!");
-    } else{
+    }else {
       Serial.print("\nFailed to initialize RFM. Try again.");
       while (true);
     }
@@ -44,7 +43,7 @@ void setup() {
     if (rf96.setFrequency(FREQ))
     {
       Serial.println("\nFrequency is set!(434.2MHz)");
-    } else{
+    }else {
       Serial.println("\nFailed to set RFM frequency to 434.2MHz");
       while (true);
     }
@@ -56,44 +55,10 @@ void setup() {
   if (scard.init(100, 7))
   {
     Serial.println("SD card initialized!");
-  } else{
+  }else {
     Serial.println("Failed to initialize SD card. Try again.");
     while (true);
   }
-
-  Serial.println("Card type:   ");
-  switch (scard.type())
-  {
-   case SD_CARD_TYPE_SD1:
-    Serial.println("SD1");
-    break;
-  
-   case SD_CARD_TYPE_SD2:
-    Serial.println("SD2");
-    break;
-
-   case SD_CARD_TYPE_SDHC:
-    Serial.println("SDHC");
-    break;
-
-   default:
-    Serial.println("Unknown");
-    break;
-  }
-
-  if (!sdsize.init(scard))
-  {
-    Serial.println("Could not find Fat16/Fat32 partition. \nMake sure the SD card is formatted");
-    while (true);
-  }
- uint32_t volumesize;
- Serial.print("Volume type is:   FAT");
- Serial.println(sdsize.fatType(), DEC);
- volumesize = sdsize.blocksPerCluster();
- volumesize *= sdsize.clusterCount();
- volumesize /= 1024;
- Serial.print("Volume size (Gb):   ");
- Serial.println((float)volumesize / 1024);
 
 if (SD.exists(DATFILE))
 {
