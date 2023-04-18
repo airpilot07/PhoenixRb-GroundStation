28f83990cdf678ecfde1f320b6883f78902ead91
airpilot07 <john.p.20071@gmail.com>
2023-04-18 18:36:41 +0300

    another commit

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <MFRC522.h>
 #include <megalovania.h>
 #include <rickroll.h>
+
 #define FREQ 434.2f //RFM frequency
 #define RFM_CS 4    //RFM chipselect pin
 #define RFM_G0 3    //RFM interrupt pin
@@ -14,23 +15,20 @@
 #define RFID_RST 6  //RFID reset pin
 #define BUZPIN 48   //Buzzer enable pin
 #define DATFILE "datatext.txt"
-#define poitxt "poi.txt"
+#define POI_TXT "poi.txt"
 
 RH_RF95 rf96(RFM_CS, RFM_G0);
 MFRC522 rfid(RFID_CS, RFID_RST);
 File coldata;
 
 bool gui = true;
-bool musicunlock = false;
+bool targetslct = false;
+bool musicunlock = true;
+String poi;
 char* packet;
-int pois = 0;
 
-void setup()
-{
-  // while (!Serial)
-  // {
-    Serial.begin(9600);
-  // }
+void setup() {
+  Serial.begin(9600);
 
   Serial.println("\nStarting Initialization");
   tone(BUZPIN, 450);
@@ -46,21 +44,17 @@ void setup()
 
   //Initialize RFM
   Serial.println("Initializing RFM. Please wait...");
-  if (rf96.init())
-  {
+  if (rf96.init()) {
     Serial.println("RFM initialized!");
-  } else
-  {
+  } else {
     Serial.println("Failed to initialize RFM. Try again.");
     while (true);
   }
 
     //Set the frequency that the RFM will use
-  if (rf96.setFrequency(FREQ))
-  {
+  if (rf96.setFrequency(FREQ)) {
     Serial.println("\nFrequency is set!(434.2MHz)");
-  } else
-  {
+  } else {
     Serial.println("\nFailed to set RFM frequency to 434.2MHz");
     while (true);
   }
@@ -68,13 +62,11 @@ void setup()
   //Initialize SD reader
   Serial.println("Initializing SD card. Please wait...");
 
-  if (SD.begin(SD_CS))
-  {
+  if (SD.begin(SD_CS)) {
     Serial.println("SD card initialized!");
-  } else
-  {
+  } else {
     Serial.println("Failed to initialize SD card. Try again.");
-    while (true);
+    //while (true);
   }
 
   //Initialize the RFID
@@ -88,71 +80,65 @@ void setup()
   noTone(BUZPIN);
 }
 
-void loop()
-{
+void loop() {
   uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
   uint8_t len = sizeof(buf);
-  if (rf96.recv(buf, &len))
-  {
+  if (rf96.recv(buf, &len)) {
     packet = (char*)buf;
-    if (packet[0] == 'P'
-        && packet[1] == 'R'
-        && packet[2] == 'b'
-        && packet[3] == '_')
-    {
-      /* code */
-    }
-    
-    if (!gui)
-    {
-      Serial.println(packet);
-    }
+    if (packet[0] == 'P' && packet[1] == 'R' && packet[2] == 'b' && packet[3] == '_') {
+      if (gui) {
+        Serial.println(packet);
+        Serial.println("RSSI: ");
+        Serial.print(rf96.lastRssi(), DEC);
+      }
 
-    coldata = SD.open(DATFILE, FILE_WRITE);
-    coldata.write((char*)buf);
-    coldata.close();
-
-    if (!gui)
-    {
-      Serial.println("RSSI: ");
-      Serial.print(rf96.lastRssi(), DEC);
+      if (packet[4] == 'D') { 
+        coldata = SD.open(DATFILE, FILE_WRITE);
+        coldata.write((char*)buf);
+        coldata.close();
+      }
+      
+      if (packet[4] == 'P') {
+        coldata = SD.open(POI_TXT, FILE_WRITE);
+        coldata.println(packet);
+        coldata.close();
+      }
     }
-
     
   }
 
-  while (!Serial.available() == 0)
-  {
-    // if (Serial.readString() == "poi")
-    // {
-    //   gui = false;
-    // }
+  while (!Serial.available() == 0) {
+    if (Serial.readString() == "poi") {
+      gui = false;
+      coldata = SD.open(POI_TXT);
+      Serial.write(coldata.read());
+      coldata.close();
+    }
 
-    // if (Serial.readString() == "music")
-    // {
-    //   musicunlock = true;
-    //   Serial.println("oki");
-    // }
+    if (Serial.readString() == "resume") {
+      gui = true;
+    }
+
+    if (Serial.readString() == "target")
+    {
+      targetslct = true;
+    }
+    
+  }
+
+  while (targetslct)
+  {
+    Serial.println("Select where to land:");
+    while (!Serial.available() == 0)
+    {
+      poi = Serial.readString();
+      uint8_t poi_def[] = "PRb_PoI", poi;
+      rf96.send(poi_def, sizeof(poi_def));
+    }
+  }
   
-    // if (Serial.readString() == "nomusic")
-    // {
-    //   musicunlock = false;
-    // }
 
-    gui = false;
-
-    coldata = SD.open(poitxt);
-    Serial.write(coldata.read());
-    coldata.close();
-  }
-
-  if (rfid.PICC_IsNewCardPresent() & musicunlock)
-  {
+  if (rfid.PICC_IsNewCardPresent() & musicunlock) {
     rickroll(BUZPIN);
   }
-  coldata = SD.open(poitxt, FILE_WRITE);
-  coldata.println("hello this is a poi");
-  coldata.print(pois = pois + 1);
-  coldata.close();
-  Serial.println("running");
 }
